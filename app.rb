e2a067d3c7fb5a4d07113e4d22f477d35d5bcec1
Yura <yura@whatever.com>
2017-03-29 14:48:09 +0300

    Add visit.erb

--- app.rb
+++ app.rb
@@ -20,10 +20,27 @@ end
 
 #Миграция схем в БД "$ rake db:migrate"
 
-get '/' do
-	#Переменной @stylists присваивается массив данных произведенных коммандой ActiveRecord
+before do
+    #Переменной @stylists присваивается массив данных произведенных коммандой ActiveRecord
 	#из таблицы с сортировкой по дате создания
 	#SELECT "stylists".* FROM "stylists" ORDER BY created_at DESC
-	@stylists=Stylist.order "created_at DESC" 
+	@stylists=Stylist.all	
+end
+
+get '/' do
 	erb :index			
+end
+
+get '/visit' do
+  erb :visit
+end
+
+post '/visit' do
+  @user_name=params[:user_name]
+  @phone=params[:phone]
+  @date_time=params[:date_time]
+  @stylist=params[:stylist]  
+  @color=params[:color]
+  
+  erb "<h2>Спасибо!Вы записаны</h2>" 
 end
