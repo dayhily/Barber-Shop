b25a7254a9002c3aab3779289c5350b69ee996b1
Yura <yura@whatever.com>
2017-03-29 13:58:38 +0300

    Add comments

--- app.rb
+++ app.rb
@@ -5,15 +5,25 @@ require 'sinatra/reloader'
 require 'sqlite3'
 require 'sinatra/activerecord'
 
+#Создание БД barbershop.db
 set :database, "sqlite3:barbershop.db"
 
+#Cоздание класса Client
 class Client < ActiveRecord::Base
 end
+#Создание схемы БД в терминале "$ rake db:create_migration NAME=create_clients"
 
+#Cоздание класса Stylist
 class Stylist < ActiveRecord::Base
 end
+#Создание схемы БД в терминале "$ rake db:create_migration NAME=create_stylist"
+
+#Миграция схем в БД "$ rake db:migrate"
 
 get '/' do
-	@stylists=Stylist.all
+	#Переменной @stylists присваивается массив данных произведенных коммандой ActiveRecord
+	#из таблицы с сортировкой по дате создания
+	#SELECT "stylists".* FROM "stylists" ORDER BY created_at DESC
+	@stylists=Stylist.order "created_at DESC" 
 	erb :index			
 end
